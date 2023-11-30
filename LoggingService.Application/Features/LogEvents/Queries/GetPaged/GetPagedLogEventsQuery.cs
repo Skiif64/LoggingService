35682f8d3c162bb33ba4ab5454eb1dd241f2f3a2
abairishevski <abairishevski@joytech.store>
2023-11-30 22:05:41 +0400

    Add GetPaged LogEventsQuery & handler

---
+++ LoggingService.Application/Features/LogEvents/Queries/GetPaged/GetPagedLogEventsQuery.cs
@@ -0,0 +1,9 @@
+﻿using LoggingService.Application.Base.Messaging;
+using LoggingService.Domain.Features.LogEvents;
+using LoggingService.Domain.Shared;
+
+namespace LoggingService.Application.Features.LogEvents.Queries.GetPaged;
+public sealed record GetPagedLogEventsQuery(string CollectionName, int PageIndex, int PageSize)
+    : IQuery<PagedList<LogEvent>>
+{
+}
---
+++ LoggingService.Application/Features/LogEvents/Queries/GetPaged/GetPagedLogEventsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using LoggingService.Application.Base.Messaging;
+using LoggingService.Domain.Features.EventCollections;
+using LoggingService.Domain.Features.LogEvents;
+using LoggingService.Domain.Shared;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingService.Application.Features.LogEvents.Queries.GetPaged;
+internal sealed class GetPagedLogEventsQueryHandler
+    : IQueryHandler<GetPagedLogEventsQuery, PagedList<LogEvent>>
+{
+    private readonly ILogEventRepository _logRepository;
+    private readonly IEventCollectionRepository _collectionRepository;
+    private readonly ILogger<GetPagedLogEventsQueryHandler> _logger;
+
+    public GetPagedLogEventsQueryHandler(ILogEventRepository logRepository,
+                                         IEventCollectionRepository collectionRepository,
+                                         ILogger<GetPagedLogEventsQueryHandler> logger)
+    {
+        _logRepository = logRepository;
+        _collectionRepository = collectionRepository;
+        _logger = logger;
+    }
+    public async Task<Result<PagedList<LogEvent>>> Handle(GetPagedLogEventsQuery request, CancellationToken cancellationToken)
+    {
+        var collection = await _collectionRepository.GetByNameAsync(request.CollectionName, cancellationToken);
+        if(collection is null)
+        {
+            _logger.LogWarning("EventCollection with name: {name} was not found", request.CollectionName);
+            return Result.Failure<PagedList<LogEvent>>(
+                EventCollectionErrors.NotFound(nameof(collection.Name), request.CollectionName));
+        }
+
+        var eventList = await _logRepository.GetPagedByCollectionIdAsync(collection.Id, request.PageIndex, request.PageSize, cancellationToken);
+        return Result.Success(eventList);
+    }
+}
