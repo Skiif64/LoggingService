f5a1ae6166d7cbf91748cf6700070af8755ad283
abairishevski <abairishevski@joytech.store>
2023-11-30 22:05:12 +0400

    Add EventCollection GetPagedQuery & handler

---
+++ LoggingService.Application/Features/EventCollections/Commands/Queries/GetPagedEventCollectionQuery.cs
@@ -0,0 +1,9 @@
+﻿using LoggingService.Application.Base.Messaging;
+using LoggingService.Domain.Features.EventCollections;
+using LoggingService.Domain.Shared;
+
+namespace LoggingService.Application.Features.EventCollections.Commands.Queries;
+public sealed record GetPagedEventCollectionQuery(int PageIndex, int PageSize)
+    : IQuery<PagedList<EventCollection>>
+{
+}
---
+++ LoggingService.Application/Features/EventCollections/Commands/Queries/GetPagedEventCollectionQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using LoggingService.Application.Base.Messaging;
+using LoggingService.Domain.Features.EventCollections;
+using LoggingService.Domain.Shared;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingService.Application.Features.EventCollections.Commands.Queries;
+internal sealed class GetPagedEventCollectionQueryHandler
+    : IQueryHandler<GetPagedEventCollectionQuery, PagedList<EventCollection>>
+{
+    private readonly IEventCollectionRepository _collectionRepository;
+    private readonly ILogger<GetPagedEventCollectionQueryHandler> _logger;
+    public GetPagedEventCollectionQueryHandler(IEventCollectionRepository collectionRepository, ILogger<GetPagedEventCollectionQueryHandler> logger)
+    {
+        _collectionRepository = collectionRepository;
+        _logger = logger;
+    }
+    public async Task<Result<PagedList<EventCollection>>> Handle(GetPagedEventCollectionQuery request, CancellationToken cancellationToken)
+    {
+        var collections = await _collectionRepository.GetPagedAsync(request.PageIndex, request.PageSize, cancellationToken);
+
+        return Result.Success(collections);
+    }
+}
