6cf17b0c6dbb8629076ff1e5d8682b3db535cd6a
abairishevski <abairishevski@joytech.store>
2023-12-01 22:31:09 +0400

    Add repositories

---
+++ LoggingService.DataAccess.Postgres/Repositories/LogEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using LoggingService.Domain.Features.LogEvents;
+using LoggingService.Domain.Shared;
+
+namespace LoggingService.DataAccess.Postgres.Repositories;
+internal sealed class LogEventRepository : BaseRepository<LogEvent>, ILogEventRepository
+{
+
+    public LogEventRepository(ApplicationDbContext context) : base(context)
+    {
+
+    }
+
+    public async Task<PagedList<LogEvent>> GetPagedByCollectionIdAsync(Guid collectionId, int pageIndex, int pageSize, CancellationToken cancellationToken)
+    {
+        var logs = Context.LogEvents
+            .Where(log => log.CollectionId == collectionId);
+
+        return await AsyncPagedList.CreateAsync(logs, pageIndex, pageSize, cancellationToken);
+    }
+}
