126b14ea3dc913b75a94e5ca147cd4077851e6ff
abairishevski <abairishevski@joytech.store>
2023-12-01 21:28:02 +0400

    Add applicationId

--- LoggingService.Domain/Features/EventCollections/EventCollection.cs
+++ LoggingService.Domain/Features/EventCollections/EventCollection.cs
@@ -4,11 +4,13 @@ using LoggingService.Domain.Features.LogEvents;
 namespace LoggingService.Domain.Features.EventCollections;
 public class EventCollection : BaseEntity
 {
-    public string Name { get; set; }
-    public IList<LogEvent> Events { get; set; } = new List<LogEvent>();
-    public EventCollection(Guid id, DateTime createdAtUtc, string name) 
+    public string Name { get; private set; }
+    public Guid? ApplicationId { get; private set; }
+    public IList<LogEvent> Events { get; private set; } = new List<LogEvent>();
+    public EventCollection(Guid id, DateTime createdAtUtc, string name, Guid? applicationId) 
         : base(id, createdAtUtc)
     {
         Name = name;
+        ApplicationId = applicationId;
     }
 }
