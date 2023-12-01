2542158cbc92526b447d044727bf28771afde6fc
abairishevski <abairishevski@joytech.store>
2023-12-01 22:30:42 +0400

    Add NotFoundException

---
+++ LoggingService.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggingService.Domain.Exceptions;
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException(Type type, object param, string paramName)
+        : base($"{type.Name} with {paramName} = {param} was not found")
+    {
+        
+    }
+}
