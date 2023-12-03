9c4fd781214547586fa04163df128002412e5d1a
abairishevski <abairishevski@joytech.store>
2023-12-03 23:19:49 +0400

    Update event collection

--- LoggingService.Domain/Features/EventCollections/EventCollection.cs
+++ LoggingService.Domain/Features/EventCollections/EventCollection.cs
@@ -1,12 +1,10 @@
 ﻿using LoggingService.Domain.Base;
-using LoggingService.Domain.Features.LogEvents;
 
 namespace LoggingService.Domain.Features.EventCollections;
 public class EventCollection : BaseEntity
 {
     public string Name { get; private set; }
     public Guid? ApplicationId { get; private set; }
-    public IList<LogEvent> Events { get; private set; } = new List<LogEvent>();
     public EventCollection(Guid id, DateTime createdAtUtc, string name, Guid? applicationId) 
         : base(id, createdAtUtc)
     {
