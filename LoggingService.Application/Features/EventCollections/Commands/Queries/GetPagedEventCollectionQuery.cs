011de9b0a476068d75473258ff479d9a6bdfe050
abairishevski <abairishevski@joytech.store>
2023-12-03 22:51:58 +0400

    Update EventCollections application logic

--- LoggingService.Application/Features/EventCollections/Commands/Queries/GetPagedEventCollectionQuery.cs
+++ LoggingService.Application/Features/EventCollections/Commands/Queries/GetPagedEventCollectionQuery.cs
@@ -4,6 +4,6 @@ using LoggingService.Domain.Shared;
 
 namespace LoggingService.Application.Features.EventCollections.Commands.Queries;
 public sealed record GetPagedEventCollectionQuery(int PageIndex, int PageSize)
-    : IQuery<PagedList<EventCollection>>
+    : IQuery<PagedList<EventCollectionDto>>
 {
 }
