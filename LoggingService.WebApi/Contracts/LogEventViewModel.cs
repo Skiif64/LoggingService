54a69584b20eb5357198a3c6a7533f4f22f8b109
abairishevski <abairishevski@joytech.store>
2023-12-03 22:53:33 +0400

    Add contracts

---
+++ LoggingService.WebApi/Contracts/LogEventViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LoggingService.Domain.Features.LogEvents;
+
+namespace LoggingService.WebApi.Contracts;
+
+public sealed class LogEventViewModel
+{
+    public required Guid Id { get; init; }
+    public required DateTime Timestamp { get; init; }
+    public required string CollectionName { get; init; }
+    public required LogEventLevel LogLevel { get; init; }
+    public required string Message { get; init; }
+    public required Dictionary<string, string> Args { get; init; }
+}
