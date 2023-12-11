98c76e2e452dd95d8daf9802e4b8276b86aa94c8
abairishevski <abairishevski@joytech.store>
2023-12-11 21:49:18 +0400

    Add cors & http logging

--- LoggingService.WebApi/Program.cs
+++ LoggingService.WebApi/Program.cs
@@ -1,14 +1,25 @@
 using LoggingService.Application;
 using LoggingService.DataAccess.Postgres;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddHttpLogging(cfg =>
+{
+    cfg.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponseStatusCode;
+});
+builder.Services.AddCors();
 builder.Services.AddApplication();
 builder.Services.AddDataAccess(builder.Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseHttpLogging();
+app.UseCors(cfg =>
+{
+    cfg.AllowAnyOrigin();
+    cfg.AllowAnyHeader();
+    cfg.AllowAnyMethod();
+});
 app.MapControllers();
 
 app.Run();
