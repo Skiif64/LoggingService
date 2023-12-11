944eb8d14fe80fca479852f69f63d4ba8d1b6548
abairishevski <abairishevski@joytech.store>
2023-12-11 22:17:49 +0400

    Add mapping configuration

---
+++ LoggingService.WebApi/Mapping/CreateLogEventToCreateLogEventDto.cs
@@ -0,0 +1,20 @@
+﻿using LoggingService.Application.Features.LogEvents.Commands;
+using LoggingService.Domain.Features.LogEvents;
+using LoggingService.WebApi.Contracts.Models;
+using Mapster;
+
+namespace LoggingService.WebApi.Mapping;
+
+public class CreateLogEventToCreateLogEventDto : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<CreateLogEventViewModel, CreateLogEventDto>()
+            .ConstructUsing(src 
+            => new CreateLogEventDto(
+                    src.LogLevel.Adapt<LogEventLevel>(),
+                    src.Timestamp,
+                    src.Message,
+                    src.Args));
+    }
+}
