b0d1cce797ec94708e20e780ab9b2bad3f70b2c7
abairishevski <abairishevski@joytech.store>
2023-12-15 22:46:02 +0400

    Add app auth & test projects

---
+++ Test/LoggingService.Application.Auth.UnitTests/TestBases/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+
+namespace LoggingService.Application.Auth.UnitTests.TestBases;
+public abstract class TestBase
+{
+    protected ILoggerFactory LoggerFactory { get; }
+    protected CancellationToken CancellationToken { get; }
+    protected IFixture Fixture { get; }
+
+    public TestBase()
+    {
+        LoggerFactory = new NullLoggerFactory();
+        CancellationToken = new CancellationToken();
+        Fixture = new Fixture()
+            .Customize(new AutoMoqCustomization());
+    }
+}
