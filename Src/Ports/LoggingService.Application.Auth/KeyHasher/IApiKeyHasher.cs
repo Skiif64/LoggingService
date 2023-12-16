d826edb9b3b21b76413c1b0fbb6a44a81ef2c758
abairishevski <abairishevski@joytech.store>
2023-12-17 00:16:40 +0400

    Add Keyhasher

---
+++ Src/Ports/LoggingService.Application.Auth/KeyHasher/IApiKeyHasher.cs
@@ -0,0 +1,9 @@
+﻿using LoggingService.Application.Authentication.Application;
+
+namespace LoggingService.Application.Auth.KeyFactory;
+
+public interface IApiKeyHasher
+{
+    (ApiKey key, string rawKey) Create(DateTime expireAtUtc);
+    bool ValidateKey(ApiKey source, string apiKey);
+}
