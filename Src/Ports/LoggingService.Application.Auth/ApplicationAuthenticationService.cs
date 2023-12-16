d73b51568c9645e710d7545ffb656ab33bf01a91
abairishevski <abairishevski@joytech.store>
2023-12-17 00:16:50 +0400

    Implement Application auth

---
+++ Src/Ports/LoggingService.Application.Auth/ApplicationAuthenticationService.cs
@@ -0,0 +1,100 @@
+﻿using LoggingService.Application.Authentication.Application;
+using LoggingService.Application.Base;
+using LoggingService.Domain.Shared;
+using LoggingService.Application.Errors;
+using LoggingService.Application.Auth.KeyFactory;
+
+namespace LoggingService.Application.Auth;
+internal sealed class ApplicationAuthenticationService : IApplicationAuthenticationService
+{
+    private const string Secret = "SomeStringThatMustBeNotHere";//TODO: remove this shit
+    private readonly IApplicationIdentityRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IApiKeyHasher _keyHasher;
+
+    public ApplicationAuthenticationService(IApplicationIdentityRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+        _keyHasher = new HMACSHA512ApiKeyHasher(Secret); //TODO: get from di
+    }
+
+    public async Task<Result<Authentication.Application.ApplicationIdentity>> GetApplicationAsync(string apiKey, CancellationToken cancellationToken = default)
+    {
+        var keyPrefix = apiKey.Split(".")[0];
+        var identity = await _repository.GetByKeyPrefixAsync(keyPrefix, cancellationToken);
+        if(identity is null)
+        {
+            return Result.Failure<Authentication.Application.ApplicationIdentity>(ApplicationIdentityErrors.NotFound);
+        }
+
+        if(!_keyHasher.ValidateKey(identity.ApiKey, apiKey))
+        {
+            return Result.Failure<Authentication.Application.ApplicationIdentity>(ApplicationIdentityErrors.KeyError);
+        }
+
+        return Result.Success(identity);
+    }
+
+    public async Task<Result<string>> RegisterApplicationAsync(string name, DateTime expireAtUtc, CancellationToken cancellationToken = default)
+    {
+        if(await _repository.ExistsByNameAsync(name, cancellationToken))
+        {
+            return Result.Failure<string>(ApplicationIdentityErrors.AlreadyExistsError(name));
+        }
+        var (apiKey, rawKey) = _keyHasher.Create(expireAtUtc);
+        var identity = Authentication.Application.ApplicationIdentity.Create(name, apiKey);
+
+        await _repository.InsertAsync(identity, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        if(_unitOfWork.SaveChangesException is not null)
+        {
+            return Result.Failure<string>(ApplicationErrors.SaveChangesError);
+        }
+        return Result.Success(rawKey);
+    }
+
+    public async Task<Result<string>> RenewApplicationAsync(Guid id, DateTime expireAtUtc, CancellationToken cancellationToken = default)
+    {
+        if(expireAtUtc < DateTime.UtcNow)
+        {
+            return Result.Failure<string>(ApplicationIdentityErrors.ArgumentError);
+        }
+        var identity = await _repository.GetByIdAsync(id, cancellationToken);
+        if(identity is null)
+        {
+            return Result.Failure<string>(ApplicationIdentityErrors.NotFound);
+        }
+        var (newKey, rawKey) = _keyHasher.Create(expireAtUtc);
+        identity.ApiKey = newKey;
+
+        _repository.Update(identity);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        if (_unitOfWork.SaveChangesException is not null)
+        {
+            return Result.Failure<string>(ApplicationErrors.SaveChangesError);
+        }
+
+        return Result.Success(rawKey);
+    }
+
+    public async Task<Result> RevokeApplicationAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var identity = await _repository.GetByIdAsync(id, cancellationToken);
+        if(identity is null)
+        {
+            return Result.Failure(ApplicationIdentityErrors.NotFound);
+        }
+
+        _repository.Delete(identity);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        if(_unitOfWork.SaveChangesException is not null)
+        {
+            return Result.Failure(ApplicationErrors.SaveChangesError);
+        }
+
+        return Result.Success();
+    }
+}
---
+++ Src/Ports/LoggingService.Application.Auth/AuthKeyAuthenticationHandler.cs
@@ -0,0 +1,47 @@
+﻿using LoggingService.Application.Authentication;
+using LoggingService.Application.Authentication.Application;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace LoggingService.Application.Auth;
+public sealed class AuthKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthenticationOptions>
+{
+    private const string AuthKeyHeader = "X-Api-Key";
+    private readonly IApplicationAuthenticationService _authService;
+    public AuthKeyAuthenticationHandler(
+        IOptionsMonitor<ApiKeyAuthenticationOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock,
+        IApplicationAuthenticationService authService)
+        : base(options, logger, encoder, clock)
+    {
+        _authService = authService;
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if(!Context.Request.Headers.TryGetValue(AuthKeyHeader, out var key) || (string?)key is null)
+        {
+            return AuthenticateResult.Fail($"Header {AuthKeyHeader} not found or key is empty.");
+        }
+
+        var getCredentialsResult = await _authService.GetApplicationAsync(key!);
+        if(!getCredentialsResult.IsSuccess)
+        {
+            return AuthenticateResult.Fail(getCredentialsResult.Error!.ToString());
+        }
+        var identity = new ClaimsIdentity(getCredentialsResult.Value!.GetClaims(), AuthenticationSchemes.ApiKeyScheme);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, AuthenticationSchemes.ApiKeyScheme);
+        return AuthenticateResult.Success(ticket);
+    }
+}
+
+public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+{
+}
