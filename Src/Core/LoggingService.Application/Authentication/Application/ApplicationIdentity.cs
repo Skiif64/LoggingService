1080388669ad2b30d3f747a48f55990410ccbab0
abairishevski <abairishevski@joytech.store>
2023-12-17 00:16:09 +0400

    Update application auth

---
+++ Src/Core/LoggingService.Application/Authentication/Application/ApplicationIdentity.cs
@@ -0,0 +1,32 @@
+﻿using LoggingService.Domain.Base;
+using System.Security.Claims;
+
+namespace LoggingService.Application.Authentication.Application;
+public class ApplicationIdentity : BaseEntity //TODO: private setters, ctor, move to domain?
+{
+    public string Name { get; set; } = null!;
+    public ApiKey ApiKey { get; set; } = null!;
+    private ApplicationIdentity()
+    {
+        
+    }
+    public Claim[] GetClaims()
+    {
+        return new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, Id.ToString()),
+            new Claim(ClaimTypes.Name, Name),
+        };
+    }
+
+    public static ApplicationIdentity Create(string name, ApiKey apiKey)
+    {       
+        return new ApplicationIdentity
+        {
+            Id = Guid.NewGuid(),
+            CreatedAtUtc = DateTime.UtcNow,
+            Name = name,
+            ApiKey = apiKey,
+        };
+    }
+}
--- Src/Core/LoggingService.Application/Authentication/Application/IApplicationAuthenticationService.cs
+++ Src/Core/LoggingService.Application/Authentication/Application/IApplicationAuthenticationService.cs
@@ -1,6 +1,37 @@
-﻿namespace LoggingService.Application.Authentication.Application;
+﻿using LoggingService.Domain.Shared;
+
+namespace LoggingService.Application.Authentication.Application;
 public interface IApplicationAuthenticationService
 {
-    Task<ApplicationCredentials?> GetCredentialsByApiKeyAsync(string apiKey, CancellationToken cancellationToken = default);
-    Task<string> RegisterApplicationAsync(string name, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Get app credentials by apiKey and validate apiKey
+    /// </summary>
+    /// <param name="apiKey">apiKey</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Result with ApplicationCredentials or error</returns>
+    Task<Result<ApplicationIdentity>> GetApplicationAsync(string apiKey, CancellationToken cancellationToken = default);
+   /// <summary>
+   /// Register new application
+   /// </summary>
+   /// <param name="name">application name</param>
+   /// <param name="expireAtUtc">Time when apiKey is expire</param>
+   /// <param name="cancellationToken"></param>
+   /// <returns>Result with apiKey or error</returns>
+    Task<Result<string>> RegisterApplicationAsync(string name, DateTime expireAtUtc, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Renew existing app credential
+    /// </summary>
+    /// <param name="id">ApplicationIdentity id</param>
+    /// <param name="expireAtUtc">New expiration time</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Result with new apiKey or error</returns>
+    Task<Result<string>> RenewApplicationAsync(Guid id, DateTime expireAtUtc, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Revoke application identity
+    /// </summary>
+    /// <param name="id">Application id</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<Result> RevokeApplicationAsync(Guid id, CancellationToken cancellationToken = default);
 }
