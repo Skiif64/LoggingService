18731062da756d14bf3327b36775e85aca96177d
abairishevski <abairishevski@joytech.store>
2024-01-02 23:40:07 +0400

    Fix view model

--- Src/LoggingService.WebApi.Contracts/Models/EventCollectionViewModel.cs
+++ Src/LoggingService.WebApi.Contracts/Models/EventCollectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace LoggingService.WebApi.Contracts.Models;
 public sealed class EventCollectionViewModel
 {
+    public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public Guid? ApplicationId { get; set; }
 }
