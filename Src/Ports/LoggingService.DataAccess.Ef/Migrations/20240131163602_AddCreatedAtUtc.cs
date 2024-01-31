612967318c5cca1dfab4a9c3f3f728c80dc5487b
Skiif <Skiif64@yandex.ru>
2024-01-31 21:58:49 +0400

    Add migration

---
+++ Src/Ports/LoggingService.DataAccess.Ef/Migrations/20240131163602_AddCreatedAtUtc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoggingService.DataAccess.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAtUtc",
+                table: "Applications",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAtUtc",
+                table: "ApiKey",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAtUtc",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAtUtc",
+                table: "ApiKey");
+        }
+    }
+}
